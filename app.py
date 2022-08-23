7f8a17336207c2a6b8cb1d557779b0eb7b38c0f1
fibarrola <fibarrola@sinc.unl.edu.ar>
2022-08-23 15:46:43 +1000

    savepoint

---
+++ app.py
@@ -0,0 +1,46 @@
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    html.Div([
+        "Input: ",
+        dcc.Input(id='my-input', value='initial value', type='text')
+    ]),
+
+    html.Br(),
+    html.Div(id='my-output'),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+@app.callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='my-input', component_property='value')
+)
+def update_output_div(input_value):
+    return f'Output: {input_value}'
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
